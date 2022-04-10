eefd697d018d382d2248bab52609b4c5e47c1fca
yannick <yannickkamdemkouam@yahoo.fr>
2022-04-10 13:26:32 +0200

    update article defilement

--- js/script.js
+++ js/script.js
@@ -5,21 +5,19 @@ const options = {
     threshold: ratio
 }
 
-const title1 = document.getElementById('h51')
-const title2 = document.getElementById('h52')
-const title3 = document.getElementById('h53')
-const title4 = document.getElementById('h54')
+// const title1 = document.getElementById('h51')
+// const title2 = document.getElementById('h52')
+// const title3 = document.getElementById('h53')
+// const title4 = document.getElementById('h54')
 
 const header = document.getElementById('header')
 const aPropos = document.getElementById('a-propos')
 const tarifs = document.getElementById('tarifs')
-const contact = document.getElementById('contact')
 
 const handleIntersect = function (entries, observer) {
     entries.forEach(function (entry) {
        if (entry.intersectionRatio >= ratio){
             
-            observerSection.unobserve(aPropos)
             entry.target.classList.remove('header-off')
             entry.target.classList.add('header-on')
        }
@@ -27,7 +25,6 @@ const handleIntersect = function (entries, observer) {
 
             entry.target.classList.remove('header-on')
             entry.target.classList.add('header-off')
-            observerSection.observe(aPropos)
        }
     })
 }
@@ -35,75 +32,75 @@ const handleIntersect = function (entries, observer) {
 const observerHeader = new IntersectionObserver(handleIntersect, options)
 observerHeader.observe(header)
 
-const handleIntersectSection = function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.intersectionRatio >= ratio) {
+// const handleIntersectSection = function (entries, observer) {
+//     entries.forEach(function (entry) {
+//         if (entry.intersectionRatio >= ratio) {
 
-            observerHeader.unobserve(header)
-            observerSection2.unobserve(tarifs)
-            entry.target.classList.remove('section-off')
-            entry.target.classList.add('section-on')
-            title1.classList.remove('title')
-        }
-       else {
+//             observerHeader.unobserve(header)
+//             observerSection2.unobserve(tarifs)
+//             entry.target.classList.remove('section-off')
+//             entry.target.classList.add('section-on')
+//             title1.classList.remove('title')
+//         }
+//        else {
 
-            entry.target.classList.remove('section-on')
-            entry.target.classList.add('section-off')
-            title1.classList.add('title')
-            observerHeader.observe(header)
-            observerSection2.observe(tarifs)
-       }  
-    })
-}
+//             entry.target.classList.remove('section-on')
+//             entry.target.classList.add('section-off')
+//             title1.classList.add('title')
+//             observerHeader.observe(header)
+//             observerSection2.observe(tarifs)
+//        }  
+//     })
+// }
 
-const observerSection = new IntersectionObserver(handleIntersectSection, options)
+// const observerSection = new IntersectionObserver(handleIntersectSection, options)
 
 
-const handleIntersectSection2 = function (entries, observer) {
-    entries.forEach(function (entry) {
-       if (entry.intersectionRatio >= ratio){
+// const handleIntersectSection2 = function (entries, observer) {
+//     entries.forEach(function (entry) {
+//        if (entry.intersectionRatio >= ratio){
             
-            observerSection.unobserve(aPropos)
-            observerSection3.unobserve(contact)
-            entry.target.classList.remove('section-off')
-            entry.target.classList.add('section-on')
-            title2.classList.remove('title')
-            title4.classList.remove('title')
-        }
-       else {
+//             observerSection.unobserve(aPropos)
+//             observerSection3.unobserve(contact)
+//             entry.target.classList.remove('section-off')
+//             entry.target.classList.add('section-on')
+//             title2.classList.remove('title')
+//             title4.classList.remove('title')
+//         }
+//        else {
 
-            entry.target.classList.remove('section-on')
-            entry.target.classList.add('section-off')
-            title2.classList.add('title')
-            title4.classList.add('title')
-            observerSection3.observe(contact)
-            observerSection.observe(aPropos)
-       }
-    })
-}
+//             entry.target.classList.remove('section-on')
+//             entry.target.classList.add('section-off')
+//             title2.classList.add('title')
+//             title4.classList.add('title')
+//             observerSection3.observe(contact)
+//             observerSection.observe(aPropos)
+//        }
+//     })
+// }
 
-const observerSection2 = new IntersectionObserver(handleIntersectSection2, options)
+// const observerSection2 = new IntersectionObserver(handleIntersectSection2, options)
 
-const handleIntersectSection3 = function (entries, observer) {
-    entries.forEach(function (entry) {
-       if (entry.intersectionRatio >= ratio){
+// const handleIntersectSection3 = function (entries, observer) {
+//     entries.forEach(function (entry) {
+//        if (entry.intersectionRatio >= ratio){
 
-            observerSection2.unobserve(tarifs)
-            entry.target.classList.remove('section-off')
-            entry.target.classList.add('section-on')
-            title3.classList.remove('title')
-       }
-       else {
+//             observerSection2.unobserve(tarifs)
+//             entry.target.classList.remove('section-off')
+//             entry.target.classList.add('section-on')
+//             title3.classList.remove('title')
+//        }
+//        else {
            
-            entry.target.classList.remove('section-on')
-            entry.target.classList.add('section-off')
-            title3.classList.add('title')
-            observerSection2.observe(tarifs)
-       }
-    })
-}
+//             entry.target.classList.remove('section-on')
+//             entry.target.classList.add('section-off')
+//             title3.classList.add('title')
+//             observerSection2.observe(tarifs)
+//        }
+//     })
+// }
 
-const observerSection3 = new IntersectionObserver(handleIntersectSection3, options)
+// const observerSection3 = new IntersectionObserver(handleIntersectSection3, options)
 
 const nav = document.getElementById('nav')
 const navOffsetTop = nav.offsetTop
@@ -127,14 +124,12 @@ const handleIntersectFooter = function (entries, footerObserver) {
     entries.forEach(function(entry) {
        if (entry.intersectionRatio >= .3){
 
-            observerSection3.unobserve(contact)
             entry.target.classList.remove('reveal-off')
             entry.target.classList.add('reveal') 
        }
        else {
             entry.target.classList.add('reveal-off')
             entry.target.classList.remove('reveal')
-            observerSection3.observe(contact)
        }
     })
 }
@@ -164,13 +159,13 @@ const changeBackground = (elements) => {
 
             setTimeout(() => {
                 element.style.backgroundImage = 'url("./ressources/nettoyage32.jpg")'
-            }, 5000)
+            }, 10000)
 
-        }, 5000) 
+        }, 10000) 
     })
 }
 changeBackground(bgHeader)
 
 setInterval(() => {
     changeBackground(bgHeader)
-}, 15000);
+}, 30000);
